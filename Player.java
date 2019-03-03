0bbe956491d80a1dd15a471259d97d33207ddeba
RSM61123 <30511320+RSM61123@users.noreply.github.com>
2019-03-03 22:23:27 +0000

    player class comments

---
+++ Player.java
@@ -0,0 +1,40 @@
+package src;
+
+//import package for scene color
+import javafx.scene.paint.Color;
+
+public class Player {
+
+    private String name;
+    private Color color;
+    private int home;
+    private int knockedOut;
+
+  //player constructor that stores parameters into local variables
+    public Player(String name, Color color, int home, int knockedOut) {
+        this.name = name;
+        this.color = color;
+        this.home = home;
+        this.knockedOut = knockedOut;
+    }
+
+    @Override
+    public String toString() {
+        return name + "/" ;
+    }
+
+  //accessor methods for name, color, home location and knocked out status
+    public String getName() {
+        return name;
+    }
+    public Color getColor() {
+        return color;
+    }
+    public int getHomeLocation() {
+        return home;
+    }
+    public int getKnockedOutLocation() {
+        return knockedOut;
+    }
+
+}
