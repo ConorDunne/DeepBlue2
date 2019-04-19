3e9f8b3520a387b8c4eb5e6b17e048ab39f39722
ConorDunne <conoredunne@gmail.com>
2019-04-19 16:50:07 +0100

    Added function calls for bot decision features

--- Sprint5/Bot0.java
+++ Sprint5/Bot0.java
@@ -36,4 +36,142 @@ public class Bot0 implements BotAPI {
         // Add your code here
         return "n";
     }
+
+//  Board Feature Calculations for Bot Decision Making
+    //  Pip Count Difference (Feature 1)
+        //  Input           - Board State
+        //  Output          - Feature score for feature 1 (between 0 and 1 where 1 is a definite win)
+            //  Equations   -   Pd = P0 - P1
+            //                  P0 = (25)E(p=0) pC0(p)
+    private int pipCountDifference() {
+        int Pd;     //  Player Difference Score
+        int P0 = 0; //  Player 0 Score
+        int P1 = 0; //  Player 1 Score
+
+        /*
+            Count board counters to get P0 and P1
+        */
+
+        Pd = (P0-P1)/375;   //  Player Difference divided by 375 (max number of points)
+        return Pd;
+    }
+
+    //  Block-Blot Difference (Feature 2)
+        //  Input           - Board State
+        //  Output          - Feature score for feature 2 (between 0 and 1 where 1 is a definite win)
+            //  Equations   -   Sd = Kx - Tx
+    private int blockBlotDifference() {
+        int Sd;
+        int Kx = 0; //  Number of Blocks by Player 0
+        int Tx = 0; //  Number of Blots by Player 1
+
+        /*
+            Count board counters to get Kx and Tx
+        */
+
+        Sd = (Kx - Tx) / 7; //  Blot-Block Difference divided by 7 (max number of points)
+        return 1;
+    }
+
+    //  Number of Homeboard Blocks (Feature 3)
+        //  Input           - Board State
+        //  Output          - Feature score for feature 3 (between 0 and 1 where 1 is a definite win)
+            //  Equations   -   H0 = (6)E(p=1) (P0(p) > 1)
+    private int homeboardBlocks() {
+        int count = 0;
+
+        /*
+            Count board counters to get count;
+        */
+
+        count /= 21;    //  Homeboard Block count divided by 20 (Max number of points)
+        return count;
+    }
+
+    //  Length of Prime with Captured Checker (Feature 4)
+        //  Input           - Board State
+        //  Output          - Feature score for feature 4 (between 0 and 1 where 1 is a definite win)
+    private int capturedPrime() {
+        int points;
+        int sizeOfPrime = 0;
+        int numberOfCaptured = 0;
+
+        /*
+            Count board counters to get sizeOfPrime and numberOfCaptured
+        */
+
+        points = (sizeOfPrime*numberOfCaptured);
+        points /= 90; //  capturedPrime points divided by 90 (max number of points)
+
+        return points;
+    }
+
+    //  Anchor Checker (Feature 5)
+        //  Input           - Board State
+        //  Output          - Feature score for feature 5 (between 0 and 1 where 1 is a definite win)
+    private int AnchorChecker() {
+        int anchorPoint = 0;
+
+        /*
+            Count board counters to get anchorPoints
+        */
+
+        return anchorPoint/21; //   anchorPoints divided by 21 (max number of points)
+    }
+
+    //  Number of Escaped Checkers (Feature 6)
+        //  Input           - Board State
+        //  Output          - Feature score for feature 6 (between 0 and 1 where 1 is a definite win)
+    private int escapedCheckers() {
+        int escaped = 0;
+
+        /*
+            Count board counters to get escaped checkers
+        */
+
+        escaped /= 15;  //  escaped divided by 15 (max number of points)
+        return escaped;
+    }
+
+    //  Number of Home Checkers (Feature 7)
+        //  Input           - Board State
+        //  Output          - Feature score for feature 7 (between 0 and 1 where 1 is a definite win)
+    private int homeCheckersNumber() {
+        int homeCheckers = 0;
+
+        /*
+            Count board counters to get home checkers
+        */
+
+        homeCheckers /= 15; //  homeCheckers divided by 15 (max number of points)
+        return homeCheckers;
+    }
+
+    //  Number of Beared Off Checkers (Feature 8)
+        //  Input           - Board State
+        //  Output          - Feature score for feature 8 (between 0 and 1 where 1 is a definite win)
+    private int bearedOffNumber() {
+        int bearOff = 0;
+
+        /*
+            Count board counters to get bear off
+        */
+
+        bearOff /= 15;  //  bearOff divided by 15 (max number of points)
+        return bearOff;
+    }
+
+    //  pointsCoveredNumbers (Feature 9)
+        //  Input           - Board State
+        //  Output          - Feature score for feature 9 (between 0 and 1 where 1 is a definite win)
+    private int pointsCovered() {
+        int pointsCovered = 0;
+
+        /*
+            Count board counters to get pointsCovered
+        */
+
+        pointsCovered /= 225; // pointsCovered divided by 225 (max number of points)
+        return pointsCovered;
+    }
 }
